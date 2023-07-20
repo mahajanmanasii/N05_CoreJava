7a2ca6b093ba3fea2ce690bd9497fd5501088ec9
mahajanmanasii <manasim.it_ioe@bkc.met.edu>
2023-07-20 12:03:40 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,30 @@
+//program to demonstrate user input
+package org.tnsif.introduction;
+
+import java.util.Scanner;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           Scanner s= new Scanner(System.in);
+           
+          /* System.out.println("Enter the value of num");
+           int num=s.nextInt();
+           System.out.println("The value of num is"+ num);
+           
+           System.out.println("Enter the value of num1");
+           float num1=s.nextFloat();
+           System.out.println("The value of num is"+ num1);
+           
+           char c=s.next().charAt(0);
+           System.out.println("Character is " +c);*/
+           
+           System.out.println("Enter the String");
+           String str1=s.nextLine();
+           s.next();
+           System.out.println("string is " + str1);
+            
+	}
+
+}
